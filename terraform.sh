adf98468fce54922c3c5f009f0e0e0635107f06d
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2026-02-21 14:29:24 -0500

    Add script to install Terraform 1.6.6

---
+++ terraform.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo dnf install -y unzip
+curl -LO https://releases.hashicorp.com/terraform/1.6.6/terraform_1.6.6_linux_amd64.zip
+unzip terraform_1.6.6_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+terraform -version
