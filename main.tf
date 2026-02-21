a88337bdc89f07c4d43a440d236baf9f6c2bf910
Shimon Brathwaite <46653837+Shimon242@users.noreply.github.com>
2026-02-21 13:11:09 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ssm_parameter" "al2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
 resource "aws_instance" "demo" {
-  ami           = "%ami-259%"
+  ami           = data.aws_ssm_parameter.al2023.value
   instance_type = "t3.micro"
 }
